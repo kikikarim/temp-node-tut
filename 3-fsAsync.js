480e5dac9280949bb8bd51af00b38ebad6a6fc96
karim guinoubi <karimguinoubi1@gmail.com>
2023-08-16 10:39:20 +0100

    modified the fsAsync file

--- 3-fsAsync.js
+++ 3-fsAsync.js
@@ -1,9 +1,21 @@
 const { readFile, writeFile } = require('fs');
 
+console.log('started a first task');
 readFile('./content/first.txt', 'utf8', (err, result) => {
     if (err) {
         console.log(err);
         return;
     }
     console.log(result);
+    console.log('completed first task');
 });
+console.log('starting next task...');
+
+/*
+console.log('first');
+setTimeout(() => {
+    console.log('second');
+}, 0);
+console.log('third'); */
+/* setTimeout() is an asynchronous function, 
+meaning that the timer function will not pause execution of other functions in the functions stack.*/
